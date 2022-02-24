efbdcfda44142acb57dce4a8b794d1e1ab69c37c
EC2 Default User <ec2-user@ip-172-31-0-195.ec2.internal>
2022-02-24 18:57:11 +0000

    wip

--- Myportfolio/src/app/app.component.ts
+++ Myportfolio/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationStart, NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Myportfolio';
+  public loading: boolean = true;
+  constructor(private router: Router) {}
+  ngOnInit() {
+    this.timeOut();
+  }
+  timeOut() {
+    setTimeout(() => {
+      this.loading = false;
+    }, 1000);
+  }
 }
